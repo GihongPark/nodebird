43f6d891fd7d04607ad7133288605fdd510a7d37
gipark <gipark1993@gmail.com>
2020-06-21 22:40:45 +0900

    프로필 추가

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -2,15 +2,22 @@ import React from 'react';
 import Head from 'next/head';
 
 import AppLayout from '../components/AppLayout';
+import NicknameEditForm from '../components/NicknameEditForm';
+import FollowList from '../components/FollowList';
 
 const Profile = () => {
+    const followingList = [{nickname: '기박'}, {nickname: '홍길동'}, {nickname: '임꺽정'},];
+    const followerList = [{nickname: '기박'}, {nickname: '홍길동'}, {nickname: '임꺽정'},];
+
     return (
         <>
             <Head>
                 <title>프로필 | Node Bird</title>
             </Head>
             <AppLayout>
-                <div>프로필 페이지</div>
+                <NicknameEditForm />
+                <FollowList header='팔로잉 목록' data={followingList} />
+                <FollowList header='팔로워 목록' data={followerList} />
             </AppLayout>
         </>
     )
